e745747f542222255b383ba59e5e797f35ed92d1
Sreenath TS <107619037+Sreenath-ts@users.noreply.github.com>
2023-05-09 10:59:22 +0530

    cors fix

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ const corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   };
 
-  app.use(cors({
-    origin: 'https://glittering-dodol-08c860.netlify.app'
+    app.use(cors({
+    origin: ['https://glittering-dodol-08c860.netlify.app','http://localhost:4200']
   }));
   
 const cookieParser = require('cookie-parser')
